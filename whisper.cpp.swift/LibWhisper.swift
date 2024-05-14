d84309796ab5da69469d0b097f08628700eb8f12
Yvan Satyawan <y_satyawan@hotmail.com>
2024-05-14 18:01:14 +1000

    Enable multi-language whisper and move out unnecessary closures

--- whisper.cpp.swift/LibWhisper.swift
+++ whisper.cpp.swift/LibWhisper.swift
@@ -22,28 +22,27 @@ actor WhisperContext {
         let maxThreads = max(1, min(8, cpuCount() - 2))
         print("Selecting \(maxThreads) threads")
         var params = whisper_full_default_params(WHISPER_SAMPLING_GREEDY)
-        "en".withCString { en in
-            // Adapted from whisper.objc
-            params.print_realtime   = true
-            params.print_progress   = false
-            params.print_timestamps = true
-            params.print_special    = false
-            params.translate        = false
-            params.language         = en
-            params.n_threads        = Int32(maxThreads)
-            params.offset_ms        = 0
-            params.no_context       = true
-            params.single_segment   = true
-            params.no_timestamps    = true
-
-            whisper_reset_timings(context)
-            print("About to run whisper_full")
-            samples.withUnsafeBufferPointer { samples in
-                if (whisper_full(context, params, samples.baseAddress, Int32(samples.count)) != 0) {
-                    print("Failed to run the model")
-                } else {
-                    whisper_print_timings(context)
-                }
+        params.print_realtime   = true
+        params.print_progress   = false
+        params.print_timestamps = true
+        params.print_special    = false
+        params.translate        = false
+        params.n_threads        = Int32(maxThreads)
+        params.offset_ms        = 0
+        params.no_context       = true
+        params.single_segment   = true
+        params.no_timestamps    = true
+        "auto".withCString { auto in
+            params.language         = auto
+            
+        }
+        whisper_reset_timings(context)
+        print("About to run whisper_full")
+        samples.withUnsafeBufferPointer { samples in
+            if (whisper_full(context, params, samples.baseAddress, Int32(samples.count)) != 0) {
+                print("Failed to run the model")
+            } else {
+                whisper_print_timings(context)
             }
         }
     }
